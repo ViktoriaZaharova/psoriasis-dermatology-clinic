d01fa97456d78907b977f77a372eb27fc69399d1
Viktoria <zaharova181114@gmail.com>
2021-12-06 20:43:25 +0200

    media2

--- app/js/common.js
+++ app/js/common.js
@@ -81,6 +81,16 @@ $('.result-slider').slick({
     nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
 });
 
+$('.license-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    dots: true,
+    appendDots: '.license-slider__nav',
+    appendArrows: '.license-slider__nav',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+});
+
 
 $('.modal').on('shown.bs.modal', function (e) {
     $('.slick-slider').slick('setPosition');
@@ -92,13 +102,7 @@ $('.btn-result-view').on('click', function (e) {
     $('.result-slider-box-before').removeClass('result-slider-box-before');
 });
 
-// range
-window.addEventListener("DOMContentLoaded", () => {
-    let range1 = new rSlider({
-        element: "#range1",
-        tick: 1
-    });
-});
+
 
 $('[name="phone"]').mask('+7 (999) 999 - 99 - 99');
 
@@ -110,6 +114,8 @@ $('.btn-close').on('click', function (e) {
     e.preventDefault();
     $('.mobile-menu').fadeOut();
 });
+
+$(".datepicker").datepicker();
 
 $(function () {
     //Сменим язык календаря на русский
@@ -134,58 +140,66 @@ $(function () {
         });
 });
 
-$(".datepicker").datepicker();
 
-class rSlider {
-    constructor(args) {
-        this.el = document.querySelector(args.element);
-        this.min = +this.el.min || 0;
-        this.max = +this.el.max || 100;
-        this.step = +this.el.step || 1;
-        this.tick = args.tick || this.step;
-        this.addTicks();
-        this.dataRange = document.createElement("div");
-        this.dataRange.className = "data-range";
-        this.el.parentElement.appendChild(this.dataRange, this.el);
-        this.updatePos();
-        this.el.addEventListener("input", () => {
-            this.updatePos();
-        });
-    }
 
-    addTicks() {
-        let wrap = document.createElement("div");
-        wrap.className = "range";
-        this.el.parentElement.insertBefore(wrap, this.el);
-        wrap.appendChild(this.el);
-        let ticks = document.createElement("div");
-        ticks.className = "range-ticks";
-        wrap.appendChild(ticks);
-        for (let t = this.min; t <= this.max; t += this.tick) {
-            let tick = document.createElement("span");
-            tick.className = "range-tick";
-            ticks.appendChild(tick);
-            let tickText = document.createElement("span");
-            tickText.className = "range-tick-text";
-            tick.appendChild(tickText);
-            tickText.textContent = t;
-        }
-    }
+// range
+// window.addEventListener("DOMContentLoaded", () => {
+//     let range1 = new rSlider({
+//         element: "#range1",
+//         tick: 1
+//     });
+// });
 
-    getRangePercent() {
-        let max = this.el.max,
-            min = this.el.min,
-            relativeValue = this.el.value - min,
-            ticks = max - min,
-            percent = relativeValue / ticks;
-        return percent;
-    }
-
-    updatePos() {
-        let percent = this.getRangePercent(),
-            left = percent * 100,
-            emAdjust = percent * 3;
-        this.dataRange.style.left = `calc(${left}% - ${emAdjust}em)`;
-        // this.dataRange.innerHTML = this.el.value;
-    }
-}
+// class rSlider {
+//     constructor(args) {
+//         this.el = document.querySelector(args.element);
+//         this.min = +this.el.min || 0;
+//         this.max = +this.el.max || 100;
+//         this.step = +this.el.step || 1;
+//         this.tick = args.tick || this.step;
+//         this.addTicks();
+//         this.dataRange = document.createElement("div");
+//         this.dataRange.className = "data-range";
+//         this.el.parentElement.appendChild(this.dataRange, this.el);
+//         this.updatePos();
+//         this.el.addEventListener("input", () => {
+//             this.updatePos();
+//         });
+//     }
+//
+//     addTicks() {
+//         let wrap = document.createElement("div");
+//         wrap.className = "range";
+//         this.el.parentElement.insertBefore(wrap, this.el);
+//         wrap.appendChild(this.el);
+//         let ticks = document.createElement("div");
+//         ticks.className = "range-ticks";
+//         wrap.appendChild(ticks);
+//         for (let t = this.min; t <= this.max; t += this.tick) {
+//             let tick = document.createElement("span");
+//             tick.className = "range-tick";
+//             ticks.appendChild(tick);
+//             let tickText = document.createElement("span");
+//             tickText.className = "range-tick-text";
+//             tick.appendChild(tickText);
+//             tickText.textContent = t;
+//         }
+//     }
+//
+//     getRangePercent() {
+//         let max = this.el.max,
+//             min = this.el.min,
+//             relativeValue = this.el.value - min,
+//             ticks = max - min,
+//             percent = relativeValue / ticks;
+//         return percent;
+//     }
+//
+//     updatePos() {
+//         let percent = this.getRangePercent(),
+//             left = percent * 100,
+//             emAdjust = percent * 3;
+//         this.dataRange.style.left = `calc(${left}% - ${emAdjust}em)`;
+//         // this.dataRange.innerHTML = this.el.value;
+//     }
+// }
