a052f877f2f57b9e3f4465cc7cbc9cb66d41a3cd
Viktoria <zaharova181114@gmail.com>
2021-12-07 10:34:17 +0200

    edit1

--- app/js/common.js
+++ app/js/common.js
@@ -35,7 +35,7 @@ $('.reviews-slider').slick({
             }
         },
         {
-            breakpoint: 576,
+            breakpoint: 577,
             settings: {
                 slidesToShow: 1,
                 arrows: false,
@@ -59,7 +59,7 @@ $('.clinics-slider').slick({
     nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
     responsive: [
         {
-            breakpoint: 576,
+            breakpoint: 577,
             settings: {
                 slidesToShow: 1,
                 variableWidth: false,
@@ -140,66 +140,22 @@ $(function () {
         });
 });
 
+//плавный скролл end
 
+$(document).ready(function () { //плавный скролл
+    $(".go_to").on("click", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
 
-// range
-// window.addEventListener("DOMContentLoaded", () => {
-//     let range1 = new rSlider({
-//         element: "#range1",
-//         tick: 1
-//     });
-// });
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
 
-// class rSlider {
-//     constructor(args) {
-//         this.el = document.querySelector(args.element);
-//         this.min = +this.el.min || 0;
-//         this.max = +this.el.max || 100;
-//         this.step = +this.el.step || 1;
-//         this.tick = args.tick || this.step;
-//         this.addTicks();
-//         this.dataRange = document.createElement("div");
-//         this.dataRange.className = "data-range";
-//         this.el.parentElement.appendChild(this.dataRange, this.el);
-//         this.updatePos();
-//         this.el.addEventListener("input", () => {
-//             this.updatePos();
-//         });
-//     }
-//
-//     addTicks() {
-//         let wrap = document.createElement("div");
-//         wrap.className = "range";
-//         this.el.parentElement.insertBefore(wrap, this.el);
-//         wrap.appendChild(this.el);
-//         let ticks = document.createElement("div");
-//         ticks.className = "range-ticks";
-//         wrap.appendChild(ticks);
-//         for (let t = this.min; t <= this.max; t += this.tick) {
-//             let tick = document.createElement("span");
-//             tick.className = "range-tick";
-//             ticks.appendChild(tick);
-//             let tickText = document.createElement("span");
-//             tickText.className = "range-tick-text";
-//             tick.appendChild(tickText);
-//             tickText.textContent = t;
-//         }
-//     }
-//
-//     getRangePercent() {
-//         let max = this.el.max,
-//             min = this.el.min,
-//             relativeValue = this.el.value - min,
-//             ticks = max - min,
-//             percent = relativeValue / ticks;
-//         return percent;
-//     }
-//
-//     updatePos() {
-//         let percent = this.getRangePercent(),
-//             left = percent * 100,
-//             emAdjust = percent * 3;
-//         this.dataRange.style.left = `calc(${left}% - ${emAdjust}em)`;
-//         // this.dataRange.innerHTML = this.el.value;
-//     }
-// }
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        var heightHeader = $('header').height();
+
+        //анимируем переход на расстояние - top за 500 мс
+        $('body,html').animate({scrollTop: top - heightHeader}, 500);
+    });
+});
